0d44f00f43787e0973be9435f89c4d9bcc48a7ab
RismaO3 <risma_oktaviani@teknokrat.ac.id>
2025-01-16 17:19:05 +0000

    risma: Upload Struktur awal

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,22 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const CONTROLLERS = _Paths.CONTROLLERS;
+  static const ADMIN = _Paths.ADMIN;
+  static const CONTACT = _Paths.CONTACT;
+  static const HOME = _Paths.HOME;
+  static const LIST = _Paths.LIST;
+  static const LOGIN = _Paths.LOGIN;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const CONTROLLERS = '/controllers';
+  static const ADMIN = '/admin';
+  static const CONTACT = '/contact';
+  static const HOME = '/home';
+  static const LIST = '/list';
+  static const LOGIN = '/login';
+}
